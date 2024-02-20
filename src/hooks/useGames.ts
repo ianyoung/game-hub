b5e5083c98f7395f820f1177ff2cf0a581f23d3e
Ian Young <66717+ianyoung@users.noreply.github.com>
2024-02-20 21:29:07 +0000

    Create useGames hook

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // Allows web requests to be aborted
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        // Check for cancelled requests
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    // Cleanup
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
