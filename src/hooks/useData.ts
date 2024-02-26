c7a72e9d04364e1366c736506e1d7ddda205c362
Ian Young <66717+ianyoung@users.noreply.github.com>
2024-02-26 20:41:10 +0000

    Add filtering by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,38 +1,48 @@
 import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import { AxiosRequestConfig, CanceledError } from 'axios';
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // Allows web requests to be aborted
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      // Allows web requests to be aborted
+      const controller = new AbortController();
 
-    setLoading(true);
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-      .then((res) => {
-        setData(res.data.results);
-        setLoading(false); // Ideally should go in 'finally'
-      })
-      .catch((err) => {
-        // Check for cancelled requests
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
+      setLoading(true);
+      apiClient
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setData(res.data.results);
+          setLoading(false); // Ideally should go in 'finally'
+        })
+        .catch((err) => {
+          // Check for cancelled requests
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setLoading(false);
+        });
 
-    // Cleanup
-    return () => controller.abort();
-  }, []);
+      // Cleanup
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, error, isLoading };
 };
